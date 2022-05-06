f860c36972c5ab08747570e4e2926133a068ea75
Guilherme Dobins <g236129@dac.unicamp.br>
2022-05-06 15:19:28 -0300

    linux port + queue lock

--- func.c
+++ func.c
@@ -3,8 +3,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <time.h>
-#include <windows.h>
-
+#include <unistd.h>
 
 //shared variables
 volatile int boarders = 0;
@@ -15,6 +14,10 @@ volatile int queue_size=0;
 //mutex locks
 pthread_mutex_t board_lock; //locks while the passenger boards, so they can update the variable and check for full car
 pthread_mutex_t unboard_lock;
+pthread_mutex_t queue_lock;
+
+pthread_mutex_t board_control;
+pthread_mutex_t unboard_control;
 
 //semaphores
 sem_t board_sem; //semaphore to allow C passengers to get in
@@ -26,20 +29,26 @@ sem_t empty_car; //semaphore to signal for an empty car and make passengers get 
 //car functions
 void load(){
     printf("Car ready, please get in\n");
+    //change sign to "Get in", in the animation
 }
 
 void unload(){
     printf("The ride is over, GET OUT!!!\n");
+    //change sign to "Get in", in the animation
 }
 
 void run(){
     printf("The car is full, so the ride is about to start\n");
     printf("Hey driver, you can run, the fifth grade is not afraid to die\n");
-    Sleep(1500);
+    usleep(1500000);
+    //change sign to "Wait for the ride to be over", in the animation
 }
 
 //passenger functions
 void board(){
+    pthread_mutex_lock(&queue_lock);
+    queue_size--;
+    pthread_mutex_unlock(&queue_lock);
     printf("Getting in uwu\n");
 }
 
@@ -54,15 +63,21 @@ void* car_func(){
         printf("%d\n", queue_size);
         ////
         load();
-        sem_post_multiple(&board_sem, C);
+        for (int i = 0; i < C; i++)
+        {
+            sem_post(&board_sem); //signals C passengers to get in
+        }
         sem_wait(&full_car);
 
         run();
 
         unload();
-        sem_post_multiple(&unboard_sem, C);
+        for (int i = 0; i < C; i++)
+        {
+            sem_post(&unboard_sem); //signals C passengers to get out
+        }
         sem_wait(&empty_car);
-        Sleep(500);
+        usleep(500000);
     }
     return NULL;
 }
@@ -74,8 +89,8 @@ void* passenger_func(){
     
     ///
     pthread_mutex_lock(&board_lock);
-    boarders++;
     board();
+    boarders++;
     if(boarders==C){
         sem_post(&full_car);
         boarders=0;
@@ -84,10 +99,9 @@ void* passenger_func(){
     ///
     
     sem_wait(&unboard_sem); //passenger must wait for ride to be over
-    unboard();
-
     //
     pthread_mutex_lock(&unboard_lock);
+    unboard();
     unboarders++;
     if (unboarders == C){
         sem_post(&empty_car);
@@ -96,7 +110,6 @@ void* passenger_func(){
     pthread_mutex_unlock(&unboard_lock);
     //
 
-    queue_size--;
     return NULL;
 }
 
@@ -110,6 +123,7 @@ void main(){
 
     pthread_mutex_init(&board_lock, NULL);
     pthread_mutex_init(&unboard_lock, NULL);
+    pthread_mutex_init(&queue_lock, NULL);
 
     sem_init(&board_sem,0,0);
     sem_init(&unboard_sem,0,0);
@@ -121,18 +135,20 @@ void main(){
     //////////////////////////////////////////////////////
     while(1){ //new passengers will join the queue indefinitely
         pthread_create(&passenger, NULL, passenger_func, NULL);
+        pthread_mutex_lock(&queue_lock);
         queue_size++;
-        Sleep(rand()%5000);
+        pthread_mutex_unlock(&queue_lock);
+        usleep(rand()%1000000);
     }
 
     //since the roller coaster never stops, this wont be executed, but in case we create a maximum number of rides, this will be necessary
     pthread_join(car, NULL);
     pthread_mutex_destroy(&board_lock);
     pthread_mutex_destroy(&unboard_lock);
+    pthread_mutex_destroy(&queue_lock);
     sem_destroy(&board_sem);
     sem_destroy(&unboard_sem);
     sem_destroy(&full_car);
     sem_destroy(&empty_car);
 
 }
-//Precisa existir algo que incremente a queue_size toda vez que uma thread de passageiro eh criada
